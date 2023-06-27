442394ad6c3c9521205ee395146ac73b391c301d
Abdul Rahman <abdul.rahman@appinventiv.com>
2023-06-28 00:44:55 +0530

    added code files

---
+++ code1.js
@@ -0,0 +1,28 @@
+const number = (prompt("Enter a positive number: "));
+let isPrime = true;
+
+if (number === 1) {
+    console.log("1 is not prime");
+}
+
+else if (number > 1) {
+
+    // looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
+
+// check if number is less than 1
+else {
+    console.log("The number is not a prime number.");
+}
